ada400ebe1626fe8ffdf3e38ccc25f13816b9239
Sahil Haridas <sahilharidas@gmail.com>
2024-12-31 13:23:06 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Notes Viewer',
+  description: 'A simple notes viewer for your Google Sheets data',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    
+      {children}
+    
+  )
+}
