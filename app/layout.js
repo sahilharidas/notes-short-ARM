45ff16c2db33e467742627339d7a79693cf2ea76
Sahil Haridas <sahilharidas@gmail.com>
2024-12-31 13:37:38 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.js
 import './globals.css'
 
 export const metadata = {
@@ -7,8 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    
-      {children}
-    
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
   )
 }
