63c2a98e2d847f193da05e99e90dea52e486369a
Sahil Haridas <sahilharidas@gmail.com>
2024-12-31 13:37:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['drive.google.com'], // Add any other image domains you'll use
   },
+  // Add these settings
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
